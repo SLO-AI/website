a3b558b1ea681384e20aec660b0f420218173848
DanielsWrath <danielhaitink@gmail.com>
2022-03-29 17:11:38 +0200

    removed function from treeview.js. Added LICENCE and adapted README.md

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# website
+# SLO-AI website
+
+This website contains the AI course information commissioned by [SLO](https://www.slo.nl).
+
+## Live version
+
+The website can be accessed at [GitHub pages](https://slo-ai.github.io/website).
+
+## Authors
+
+John Val
+
+Daniël Haitink
+
+Hennie Huijgens
+
+Hans van den Burg
+
+Tim Steenvoorden
+
+Jochem Baas
--- treeview.js
+++ treeview.js
@@ -15,15 +15,3 @@ function initTreeview()
 	  });
 	}
 }
-/*
- * addPathLead adds a path in leading to this website to the path to a page
- * present in this website.
- */
-function addPathLead(elemid="path_lead")
-{
-	// change the path_lead variable to your needs
-	const path_lead='<a href="https://www.johnval.nl"  target="_parent">John Val</a>/<a href="../../informatica/index.htm" target="_parent">Informatica</a>/';
-	let obj = document.getElementById(elemid);
-	if(obj)	obj.innerHTML = path_lead+obj.innerHTML;
-	
-}
